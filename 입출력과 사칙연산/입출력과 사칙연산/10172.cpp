c59334e4f5a1730af036fca70f06be9854e69fe9
hyeon12 <56420106+hyeon12@users.noreply.github.com>
2021-01-19 16:39:19 +0900

    4

---
+++ 입출력과 사칙연산/입출력과 사칙연산/10172.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "|\\_/|" << endl;
+	cout << "|q p|   /}" << endl;
+	cout << "( 0 )\"\"\"\\" << endl;
+	cout << "|\"^\"`    |" << endl;
+	cout << "||_/=\\\\__|" << endl;
+}
+
+/*
+""는 출력문안에 내용물을 쓸때 사용되므로 "만 사용해서는 출력문에 나오지 않기때문에
+내가 원하는 " 그대로 출력할려면 \을 앞에 입력하여 기능을 취소해야 한다.
+*/
