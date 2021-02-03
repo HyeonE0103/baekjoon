8516d3b58fc5fde74f54816cefb8850dd94df1bf
hyeon12 <56420106+hyeon12@users.noreply.github.com>
2021-02-03 22:13:42 +0900

    미완료

---
+++ 기본수학1/기본수학1/1712.cpp
@@ -0,0 +1,15 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;	//a=고정비용, b=가변비용(생산비?), c=물건 값
+	if (c <= b)	//이익이 가변비용보다 적거나 같으면 손익분기점을 넘을 수가 없음
+		cout << "-1" << endl;
+	else {
+		cout << (a / (c - b)) + 1 << endl;
+	}	// c-b = 물건 값에서 가변비용 뺀 판매순익
+		//고정비용/판매순익을 한 후 +1을 하면 순익분기점이 넘을 때 판매량
+}
+*/
---
+++ 기본수학1/기본수학1/2292.cpp
@@ -0,0 +1,14 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, num = 1;
+	cin >> n;
+	for (int i = 1; 1 < n; i++) {
+		n = n - (i * 6);
+		num++;
+	}
+	cout << num << endl;
+}
+*/
