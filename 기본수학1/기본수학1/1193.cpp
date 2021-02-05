e67217e6fe9e7d4d38f4fe4b86c24003c66de68d
hyeon12 <56420106+hyeon12@users.noreply.github.com>
2021-02-05 21:18:02 +0900

    미완료

--- 기본수학1/기본수학1/1193.cpp
+++ 기본수학1/기본수학1/1193.cpp
@@ -1,6 +1,30 @@
+/*
 #include <iostream>
 using namespace std;
 
 int main() {
-
+	int n, a = 1, k = 1;
+	cin >> n;
+	for (int i = 0; i < n-1; i++) {
+		if (k % 2 != 0) {
+			if (a == 1) {
+				k++;
+				a = 1;
+			}
+			else
+				a--;
+		}
+		else {
+			if (a == k) {
+				k++;
+				a = k;
+			}
+			else
+				a++;
+		}
+	}
+	cout << a << "/" << k + 1 - a << "\n";
 }
+//a는 1, 1,2, 3,2,1, 1,2,3,4..  식으로 진행되니 홀수번째에는 오름차순, 홀수번째는 내림차순으로 진행
+								개수는 1->2->3->4로 진행되어 그것을 이용
+*/
---
+++ 기본수학1/기본수학1/2869.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);	//속도 향샹을 위해
+	
+	int a, b, c, v;
+	cin >> a >> b >> v;
+	c
+	
+}
