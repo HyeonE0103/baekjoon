8b27e3dfb42339602c1275c51aab875240471ffb
hyeon12 <56420106+hyeon12@users.noreply.github.com>
2021-03-05 20:47:05 +0900

    완료

--- 기본수학1/기본수학1/10757.cpp
+++ 기본수학1/기본수학1/10757.cpp
@@ -1,65 +1,52 @@
-#include <iostream>	//틀림 하지만 반례를 못찾음
-#include <vector>	//틀린 이유를 모르겠음....
-using namespace std;	//큰수의 합은 c++로 범위가 안되서 문자열로 받아서 더함
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
 
 int main() {
-	string  a, b;
-	int num=0;
-	vector <int> c, d, v;
-	cin >> a >> b;
-	if (a.size() >= b.size()) {
-		for (int i = (a.size()-1); i >= 0; i--)
-			c.push_back(int(a[i])-48);
-		for (int i = (b.size()-1); i >= 0; i--)
-			d.push_back(int(b[i])-48);
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n = 0;
+	string n1, n2;
+	vector<int>a, b, c;
+	cin >> n1 >> n2;
+	if (n1.size() >= b.size()) {
+		for (int i = n1.size() - 1; i >= 0; i--)	//문자열이기 때문에 마지막은 \0
+			a.push_back(n1[i] - '0');
+		for (int i = n2.size() - 1; i >= 0; i--)
+			b.push_back(n2[i] - '0');
 	}
 	else {
-		for (int i = (a.size() - 1); i >= 0; i--)
-			d.push_back(int(a[i]) - 48);
-		for (int i = (b.size() - 1); i >= 0; i--)
-			c.push_back(int(b[i]) - 48);
+		for (int i = n1.size() - 1; i >= 0; i--)
+			b.push_back(n1[i] - '0');
+		for (int i = n2.size() - 1; i >= 0; i--)
+			a.push_back(n2[i] - '0');
 	}
-
-	if (c.size() > d.size()) {
-		for (int i = 0; i < d.size(); i++) {
-			if (c[i] + d[i] + num >= 10) {
-				v.push_back(c[i] + d[i] - 10 + num);
-				num = 1;
+	for (int i = 0; i < b.size(); i++) {
+		if (a[i] + b[i] + n < 10) {
+			c.push_back(a[i] + b[i] + n);
+			n = 0;
+		}
+		else {
+			c.push_back(a[i] + b[i] + n - 10);
+			n = 1;
+		}
+	}
+	if (a.size() != b.size()) {
+		for (int i = b.size(); i < a.size(); i++) {
+			if (a[i] + n < 10) {
+				c.push_back(a[i] + n);
+				n = 0;
 			}
 			else {
-				v.push_back(c[i] + d[i] + num);
-				num = 0;
+				c.push_back(a[i] + n - 10);
+				n = 1;
 			}
 		}
-		for (int i = d.size(); i < c.size(); i++) {
-			if (c[i] + num >= 10) {
-				v.push_back(c[i] + num -10);
-				num = 1;
-			}
-			else {
-				v.push_back(c[i] + num);
-				num = 0;
-			}
-		}
-		if (num == 1)
-			v.push_back(1);
 	}
-	if (c.size() == d.size()) {
-		for (int i = 0; i < d.size(); i++) {
-			if (c[i] + d[i] >= 10) {
-				v.push_back(c[i] + d[i] - 10 + num);
-				num = 1;
-			}
-			else {
-				v.push_back(c[i] + d[i] + num);
-				num = 0;
-			}
-		}
-		if (num == 1)
-			v.push_back(1);
-	}
-	
-	for (int i = (v.size()-1); i >= 0; i--)
-		cout << v[i];
-	cout << '\n';
+	if (n == 1)	
+		c.push_back(1);
+	for (int i = c.size() - 1; i >= 0; i--)
+		cout << c[i];
 }
